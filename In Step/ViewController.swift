a5d044e3a0ef8f47a976c98eaa10bdbed88e779e
Garrett Votaw <Garrett@Garretts-MacBook-Pro.local>
2017-05-15 22:54:25 -0500

    Initial Commit

---
+++ In Step/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  In Step
+//
+//  Created by Garrett Votaw on 5/15/17.
+//  Copyright © 2017 Garrett Votaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
